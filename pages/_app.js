03b16b8aae231ec614a91053a3da867cd82828c4
Andrei Mosica <andrei.mosica@gmail.com>
2022-03-05 17:45:16 +0200

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,4 @@
 import "assets/styles/main.scss";
-import Layout from "layouts/default";
-
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <Layout>
-//       <Component {...pageProps} />
-//     </Layout>
-//   );
-// }
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
